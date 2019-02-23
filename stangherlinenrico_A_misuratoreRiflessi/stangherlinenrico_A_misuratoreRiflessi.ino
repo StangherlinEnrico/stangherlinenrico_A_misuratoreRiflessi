0128e7149e41c830bbcd94041baeef7ef410db9b
Enrico <enrico.stangherlin@barsanti.edu.it>
2019-02-23 21:39:59 +0100

    Update
    Creazione del circuito sulla breadboard.
    Studio sull'utilizzo del cicalino e del display LCD a 12 collegamenti.
    Inizio creazione del programma per la gestione del Led RGB e del cicalino per il quanto di tempo da quando il pulsante è premuto.
    23/02/2019 - 21:39

--- stangherlinenrico_A_misuratoreRiflessi/stangherlinenrico_A_misuratoreRiflessi.ino
+++ stangherlinenrico_A_misuratoreRiflessi/stangherlinenrico_A_misuratoreRiflessi.ino
@@ -1,9 +1,58 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "pitches.h"
 
+int pinButton = 2;
+int delayRestart = 5000;
+
+// VARIABILI RGB
+int pinBlue = 3;
+int pinGreen = 4;
+int pinRed = 5;
+int delayBlue = random(1500);
+int attesaBlue = 0;
+// END
+
+// VARIABILI CICALINO
+int pinCicalino = 6;
+int melodia = NOTE_B5;
+int delayCic = random(1500);
+int attesaCic = 0;
+// END
+
+void riflessiLed()
+{
+  delay(delayBlue);
+  digitalWrite(pinBlue, HIGH);
+  while(digitalRead(pinButton) == LOW)
+  {
+    attesaBlue++;
+    delay(1);
+  }
+  digitalWrite(pinBlue, LOW);
+}
+
+void riflessiCic()
+{
+  delay(delayCic);
+  while(digitalRead(pinButton) == LOW)
+  {
+    attesaCic++;
+    tone(pinCicalino, melodia, 1);
+  }
+  while(digitalRead(pinButton) == HIGH) {}
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinButton, INPUT);
+  pinMode(pinBlue, OUTPUT);
+  pinMode(pinGreen, OUTPUT);
+  pinMode(pinRed, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while(digitalRead(pinButton) == LOW) {}
+  riflessiLed();
+  riflessiCic();
+  Serial.println("Stampa dei risultati. Premere nuovamente per uscire");
+  while(digitalRead(pinButton) == LOW) {}
 }
