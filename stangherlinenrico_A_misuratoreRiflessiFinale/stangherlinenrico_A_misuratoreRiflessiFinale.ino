536611d45aad516117739ea424d89435198c0baf
Enrico <enrico.stangherlin@barsanti.edu.it>
2019-03-02 11:48:46 +0100

    UpdateFinale
    Creazione della funzione per la scelta dell'azione da eseguire.
    02/03/2019 - 11:48

--- stangherlinenrico_A_misuratoreRiflessiFinale/stangherlinenrico_A_misuratoreRiflessiFinale.ino
+++ stangherlinenrico_A_misuratoreRiflessiFinale/stangherlinenrico_A_misuratoreRiflessiFinale.ino
@@ -8,21 +8,26 @@ String opzioni[4] { "  Run Program", "   randomTime", "Promotion times", "      
 int tmpRandom[4] { 3000, 4000, 5000, 6000 };
 int esecuzione = 0;
 
-int pinButtonPrec = 9;
-int pinButtonOk = 10;
-int pinButtonNext = 11;
-int randomTime = random(3000, 6000);
+int randomTime = random(3000, 6000); // Genera numero random base
+
+// VARIABILI JOYSTICK
+int varX = 0;
+int varY = 1;
+int okClick = 12;
+// END
 
 // VARIABILI LED
-int pinRed = 8; // Pin rosso
+int pinRed = 8; // Pin Red
+int pinGreen = 9; // Pin Green
+int pinBlue = 10; // Pin Blue
 int attesaLed = 0; // Variabile per salvare il tempo che la persona ha impiegato per premere il pulsante
-int promozioneLed = 800;
+int promozioneLed = 800; // Tempo base per la promozione Led
 // END
 
 // VARIABILI CICALINO
 int pinCicalino = 12; // Pin del cicalino
 int attesaCic = 0; // Variabile per salvare il tempo che la persona ha impiegato per premere il pulsante
-int promozioneCic = 1100;
+int promozioneCic = 1100; // Tempo base per la promozione Cic
 // END
 
 // VARIABILI DISPLAY CLD
@@ -35,8 +40,8 @@ int sceltaFunzione()
   bool Ciclo = true;
   while(Ciclo)
   {
-    while(digitalRead(pinButtonPrec) == LOW and digitalRead(pinButtonOk) == LOW and digitalRead(pinButtonNext) == LOW){}
-    if (digitalRead(pinButtonPrec) == HIGH)
+    while(digitalRead(okClick) == LOW and analogRead(varX) > 256 and analogRead(varX) < 768){}
+    if (analogRead(varX) < 256)
     {
       if (Pos == 0)
         Pos = 2;
@@ -47,7 +52,7 @@ int sceltaFunzione()
       lcd.setCursor(0, 1);
       lcd.print(opzioni[Pos]);
     }
-    else if (digitalRead(pinButtonNext) == HIGH)
+    else if (analogRead(varX) > 768)
     {
       if (Pos == 2)
         Pos = 0;
@@ -58,354 +63,50 @@ int sceltaFunzione()
       lcd.setCursor(0, 1);
       lcd.print(opzioni[Pos]);
     }
-    else
+    else if (digitalRead(okClick) == HIGH)
       Ciclo = false;
-    while(digitalRead(pinButtonPrec) == HIGH or digitalRead(pinButtonOk) == HIGH or digitalRead(pinButtonNext) == HIGH){}
+    while(digitalRead(okClick) == HIGH or analogRead(varX) < 256 or analogRead(varX) > 768){}
   }
   return Pos;
 }
 
 void startProgram()
 {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("TL:");
-  lcd.setCursor(0, 1);
-  lcd.print("TC:");
-  delay(randomTime);
-  digitalWrite(pinRed, HIGH);
-  while(digitalRead(pinButtonOk) == LOW)
-  {
-    attesaLed++;
-    delay(1);
-    lcd.setCursor(4, 0);
-    lcd.print(attesaLed);
-  }
-  while(digitalRead(pinButtonOk) == HIGH);
-  digitalWrite(pinRed, LOW);
-  delay(randomTime);
-  while(digitalRead(pinButtonOk) == LOW)
-  {
-    attesaCic++;
-    tone(pinCicalino, NOTE_C6, 1);
-    lcd.setCursor(4, 1);
-    lcd.print(attesaCic);
-  }
-  while(digitalRead(pinButtonOk) == HIGH);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("L: "); lcd.print(attesaLed); lcd.print("|C: "); lcd.print(attesaCic);
-  lcd.setCursor(0, 1);
-  if (attesaLed < promozioneLed and attesaCic < promozioneCic)
-  {
-    lcd.print("Congratulazioni!");
-    delay(5000);
-  }
-  else
-  {
-    lcd.print("   FALLIMENTO");
-    delay(5000);
-  }
-  attesaLed = attesaCic = 0;
+  
 }
 
 void modificaTmpRandom()
 {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("< TEMPO MINIMO >");
-  lcd.setCursor(0, 1);
-  lcd.print("      "); lcd.print(tmpRandom[0]);
-  int nMin = 0; int nMax = 0; int Pos = 0; int Pos2 = 0;
-  bool valenzaNumeri = true;
-  while(valenzaNumeri)
-  {
-    while(digitalRead(pinButtonPrec) == LOW and digitalRead(pinButtonOk) == LOW and digitalRead(pinButtonNext) == LOW){}
-    if (digitalRead(pinButtonPrec) == HIGH)
-    {
-      if (Pos == 0)
-        Pos = 2;
-      else
-        Pos--;
-      lcd.setCursor(0, 1);
-      lcd.print(opzioni[3]);
-      lcd.setCursor(0, 1);
-      lcd.print("      "); lcd.print(tmpRandom[Pos]);
-    }
-    else if (digitalRead(pinButtonNext) == HIGH)
-    {
-      if (Pos == 2)
-        Pos = 0;
-      else
-        Pos++;
-      lcd.setCursor(0, 1);
-      lcd.print(opzioni[3]);
-      lcd.setCursor(0, 1);
-      lcd.print("      "); lcd.print(tmpRandom[Pos]);
-    }
-    else
-    {
-      nMin = tmpRandom[Pos];
-      valenzaNumeri = false;
-    }
-    while(digitalRead(pinButtonPrec) == HIGH or digitalRead(pinButtonOk) == HIGH or digitalRead(pinButtonNext) == HIGH){}
-  }
-  valenzaNumeri = true;
-  if (Pos == 2)
-    nMax = tmpRandom[3];
-  else
-  {
-    Pos2 = (Pos + 1);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("<NUMERO MASSIMO>");
-    lcd.setCursor(0, 1);
-    lcd.print("      "); lcd.print(tmpRandom[(Pos + 1)]);
-    while(valenzaNumeri)
-    {
-      while(digitalRead(pinButtonPrec) == LOW and digitalRead(pinButtonOk) == LOW and digitalRead(pinButtonNext) == LOW){}
-      if (digitalRead(pinButtonPrec) == HIGH)
-      {
-        if (Pos2 == (Pos + 1))
-          Pos2 = 3;
-        else
-          Pos2--;
-        lcd.setCursor(0, 1);
-        lcd.print(opzioni[3]);
-        lcd.setCursor(0, 1);
-        lcd.print("      "); lcd.print(tmpRandom[Pos2]);
-      }
-      else if (digitalRead(pinButtonNext) == HIGH)
-      {
-        if (Pos2 == 3)
-          Pos2 = (Pos + 1);
-        else
-          Pos2++;
-        lcd.setCursor(0, 1);
-        lcd.print(opzioni[3]);
-        lcd.setCursor(0, 1);
-        lcd.print("      "); lcd.print(tmpRandom[Pos2]);
-      }
-      else
-      {
-        nMax = tmpRandom[Pos2];
-        valenzaNumeri = false;
-      }
-      while(digitalRead(pinButtonPrec) == HIGH or digitalRead(pinButtonOk) == HIGH or digitalRead(pinButtonNext) == HIGH){}
-    }
-  }
-  randomTime = random(nMin, nMax);
+  
 }
 
 bool verificaCodice()
 {
-  int num = 0; String codice = "";
-  bool ciclo = true;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("SECRET CODE-1234");
-  lcd.setCursor(0, 1);
-  lcd.print("      "); lcd.print("0000");
-
-  for (int i = 6; i < 10; i++)
-  {
-    num = 0;
-    while(ciclo)
-    {
-      while(digitalRead(pinButtonPrec) == LOW and digitalRead(pinButtonOk) == LOW and digitalRead(pinButtonNext) == LOW){}
-      if (digitalRead(pinButtonPrec) == HIGH)
-      {
-        if (num == 0)
-          num = 9;
-        else
-          num--;
-        lcd.setCursor(i, 1);
-        lcd.print(num);
-      }
-      else if (digitalRead(pinButtonNext) == HIGH)
-      {
-        if (num == 9)
-          num = 0;
-        else
-          num++;
-        lcd.setCursor(i, 1);
-        lcd.print(num);
-      }
-      else
-      {
-        if (i == 6)
-          codice = num;
-        else
-          codice += num;
-        ciclo = false;
-      }
-      while(digitalRead(pinButtonPrec) == HIGH or digitalRead(pinButtonNext) == HIGH or digitalRead(pinButtonOk) == HIGH){}
-    }
-    ciclo = true;
-  }
-  if (codice == "1234")
-    return true;
-  else
-  {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(" CODICE ERRATO!");
-    lcd.setCursor(0, 1);
-    lcd.print("   Ok to exit");
-    while(digitalRead(pinButtonOk) == LOW);
-    return false;
-  }
+  
 }
 
 void modificaCriteriPromozione()
 {
-  int num = 0; String blu = ""; String cic = "";
-  bool ciclo = true;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("ledTimePromotion");
-  lcd.setCursor(0, 1);
-  lcd.print("      "); lcd.print("0800");
-
-  for (int i = 6; i < 10; i++)
-  {
-    if (i == 7)
-      num = 8;
-    else
-      num = 0;
-    while(ciclo)
-    {
-      while(digitalRead(pinButtonPrec) == LOW and digitalRead(pinButtonOk) == LOW and digitalRead(pinButtonNext) == LOW){}
-      if (digitalRead(pinButtonPrec) == HIGH)
-      {
-        if (num == 0)
-          num = 9;
-        else
-          num--;
-        lcd.setCursor(i, 1);
-        lcd.print(num);
-      }
-      else if (digitalRead(pinButtonNext) == HIGH)
-      {
-        if (num == 9)
-          num = 0;
-        else
-          num++;
-        lcd.setCursor(i, 1);
-        lcd.print(num);
-      }
-      else
-      {
-        if (i == 6)
-          blu = num;
-        else
-          blu += num;
-        ciclo = false;
-      }
-      while(digitalRead(pinButtonPrec) == HIGH or digitalRead(pinButtonNext) == HIGH or digitalRead(pinButtonOk) == HIGH){}
-    }
-    ciclo = true;
-  }
-  if (blu == "0000")
-  {
-    lcd.clear();
-    lcd.setCursor(4, 0);
-    lcd.print("ERROR!");
-    lcd.setCursor(0, 1);
-    lcd.print(" ledProm <= 800");
-    delay(1200);
-  }
-  for (int i = 0; i < 4; i++)
-    if (blu[0] == "0")
-      blu.remove(0, 1);
-
-
-  num = 0;
-  ciclo = true;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("ledCicaPromotion");
-  lcd.setCursor(0, 1);
-  lcd.print("      "); lcd.print("1100");
-
-  for (int i = 6; i < 10; i++)
-  {
-    if (i == 6 or i == 7)
-      num = 1;
-    else
-      num = 0;
-    while(ciclo)
-    {
-      while(digitalRead(pinButtonPrec) == LOW and digitalRead(pinButtonOk) == LOW and digitalRead(pinButtonNext) == LOW){}
-      if (digitalRead(pinButtonPrec) == HIGH)
-      {
-        if (num == 0)
-          num = 9;
-        else
-          num--;
-        lcd.setCursor(i, 1);
-        lcd.print(num);
-      }
-      else if (digitalRead(pinButtonNext) == HIGH)
-      {
-        if (num == 9)
-          num = 0;
-        else
-          num++;
-        lcd.setCursor(i, 1);
-        lcd.print(num);
-      }
-      else
-      {
-        if (i == 6)
-          cic = num;
-        else
-          cic += num;
-        ciclo = false;
-      }
-      while(digitalRead(pinButtonPrec) == HIGH or digitalRead(pinButtonNext) == HIGH or digitalRead(pinButtonOk) == HIGH){}
-    }
-    ciclo = true;
-  }
-  if (cic == "0000")
-  {
-    lcd.clear();
-    lcd.setCursor(4, 0);
-    lcd.print("ERROR!");
-    lcd.setCursor(0, 1);
-    lcd.print(" cicProm < 1100");
-    delay(1200);
-  }
-  for (int i = 0; i < 4; i++)
-    if (cic[0] == "0")
-      cic.remove(0, 1);
-
-  promozioneLed = atol(blu.c_str());
-  promozioneCic = atol(cic.c_str());
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("  Data changed");
-  lcd.setCursor(0, 1);
-  lcd.print("   Ok to exit");
-  while(digitalRead(pinButtonOk) == LOW){}
+  
 }
 
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
   lcd.begin(16, 2); // Inizializzo il display LCD
   // ASSEGNO GLI INPUT E GLI OUTPUT
-  pinMode(pinButtonPrec, INPUT);
-  pinMode(pinButtonOk, INPUT);
-  pinMode(pinButtonNext, INPUT);
   pinMode(pinRed, OUTPUT);
-  pinMode(pinCicalino, OUTPUT);
+  pinMode(pinGreen, OUTPUT);
+  pinMode(pinBlue, OUTPUT);
+  pinMode(okClick, INPUT);
   lcd.setCursor(0, 0);
   lcd.print("<    SELECT    >");
   lcd.setCursor(0, 1);
   lcd.print(opzioni[0]);
   esecuzione = sceltaFunzione();
 }
-
-void loop() {
+void loop()
+{
   if (esecuzione == 0)
     startProgram();
   else if (esecuzione == 1)
@@ -416,6 +117,6 @@ void loop() {
     if (codice)
       modificaCriteriPromozione();
   }
-  while(digitalRead(pinButtonOk) == HIGH){}
+  while(digitalRead(okClick) == HIGH){}
   setup();
 }
